98a59cf2f1683c76a6b3e571d74f39ce6a21d733
YongHyun Park <67228941+enkeejunior1@users.noreply.github.com>
2022-01-26 09:54:36 +0900

    LearnUE4, Breden 추가.

---
+++ LearnUE4/Intermediate/Build/Win64/LearnUE4Editor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30136
+#include "C:/nolme/Learn/LearnUE4/Intermediate/Build/Win64/LearnUE4Editor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.h"
---
+++ LearnUE4/Intermediate/Build/Win64/UE4Editor/Inc/LearnUE4/LearnUE4GameMode.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LEARNUE4_LearnUE4GameMode_generated_h
+#error "LearnUE4GameMode.generated.h already included, missing '#pragma once' in LearnUE4GameMode.h"
+#endif
+#define LEARNUE4_LearnUE4GameMode_generated_h
+
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_SPARSE_DATA
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_RPC_WRAPPERS
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALearnUE4GameMode(); \
+	friend struct Z_Construct_UClass_ALearnUE4GameMode_Statics; \
+public: \
+	DECLARE_CLASS(ALearnUE4GameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LearnUE4"), LEARNUE4_API) \
+	DECLARE_SERIALIZER(ALearnUE4GameMode)
+
+
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesALearnUE4GameMode(); \
+	friend struct Z_Construct_UClass_ALearnUE4GameMode_Statics; \
+public: \
+	DECLARE_CLASS(ALearnUE4GameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LearnUE4"), LEARNUE4_API) \
+	DECLARE_SERIALIZER(ALearnUE4GameMode)
+
+
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	LEARNUE4_API ALearnUE4GameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALearnUE4GameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(LEARNUE4_API, ALearnUE4GameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALearnUE4GameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	LEARNUE4_API ALearnUE4GameMode(ALearnUE4GameMode&&); \
+	LEARNUE4_API ALearnUE4GameMode(const ALearnUE4GameMode&); \
+public:
+
+
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	LEARNUE4_API ALearnUE4GameMode(ALearnUE4GameMode&&); \
+	LEARNUE4_API ALearnUE4GameMode(const ALearnUE4GameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(LEARNUE4_API, ALearnUE4GameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALearnUE4GameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ALearnUE4GameMode)
+
+
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_9_PROLOG
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_SPARSE_DATA \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_RPC_WRAPPERS \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_INCLASS \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_SPARSE_DATA \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_INCLASS_NO_PURE_DECLS \
+	LearnUE4_Source_LearnUE4_LearnUE4GameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LEARNUE4_API UClass* StaticClass<class ALearnUE4GameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LearnUE4_Source_LearnUE4_LearnUE4GameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS

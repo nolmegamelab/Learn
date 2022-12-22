795ab1b7c4cff7984ee8e3b4d6996ee88bd6083f
nolmegamelab <nolmegamelab@gmail.com>
2022-12-22 16:45:40 +0900

    추가 안 된 파일들 추가.

---
+++ LearnRust/boxing/src/main.rs
@@ -0,0 +1,41 @@
+enum List {
+    Cons(i32, Box<List>), 
+    Nil
+}
+
+// use crate means this crate.
+use crate::List::{Cons, Nil};
+
+fn test_list() {
+    let lst : List = Cons(3, Box::new(Cons(5, Box::new(Nil))));
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn test_deref() {
+    let x = 5;
+    let y : MyBox<i32> = MyBox::new(x);
+
+    assert_eq!(x, *y);
+}
+
+fn main() {
+   test_list(); 
+   test_deref();
+}
